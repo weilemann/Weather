08aaa928ba77bab01dcbaf9b952aae152dc89534
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-01-29 09:37:27 -0300

    Removing old key

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const API_KEY = '8q0uldk3HqicmzdWe7WE4e61naSl1zkZfo2EGi3J'
+const API_KEY = 'DEMO_KEY'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
