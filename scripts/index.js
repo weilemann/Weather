7892cbe4ed8029ae04a4cbe18d8dcc26583a85e5
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-02-01 17:34:40 -0300

    Removing comment

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const API_KEY = 'DEMO_KEY' // Insert your key here
+const API_KEY = 'DEMO_KEY'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
